bcf9fac4ab732161bd2396c762d1ae063777ce30
Petko Dapchev <p.dapchev09@gmail.com>
2021-02-23 16:15:00 +0200

    Version with picture from gallery

--- project/Screens/PictureSharingViewController.swift
+++ project/Screens/PictureSharingViewController.swift
@@ -11,32 +11,29 @@ import SwiftyCam
 class PictureSharingViewController: SwiftyCamViewController, SwiftyCamViewControllerDelegate{
     
     
+    @IBOutlet weak var img: UIImageView!
+    
     let image = UIImage(named: "Image")
 
         
-          
-    @IBOutlet weak var camButton: SwiftyCamButton!
     
+    @IBOutlet weak var camButton: SwiftyCamButton!
+    let imagePicker = UIImagePickerController()
     
 override func viewDidLoad(){
     
     
-   // let imageToShare = [ image! ]
+    let imageToShare = [ img! ]
     
-    //let activityViewController = UIActivityViewController(activityItems: imageToShare, applicationActivities: nil)
+    
     
     super.viewDidLoad()
     cameraDelegate = self
-    
+    imagePicker.delegate = self
         isPressed(camButton)
         
     
-    //activityViewController.popoverPresentationController?.sourceView = self.view // so that iPads won't crash
     
-   // activityViewController.excludedActivityTypes = [ UIActivity.ActivityType.airDrop, UIActivity.ActivityType.postToFacebook ]
-
-    // present the view controller
-   // self.present(activityViewController, animated: true, completion: nil)
     
     
 }
@@ -50,4 +47,37 @@ override func viewDidLoad(){
          // Returns a UIImage captured from the current session
     }
     
+    @IBAction func buttonOnPressBrowsePicture(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = true
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func sharePicture(_ sender: Any) {
+       
+        
+        UIGraphicsBeginImageContext(img.frame.size)
+        img.layer.render(in: UIGraphicsGetCurrentContext()!)
+        print("Sharing image")
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        
+        let activityViewController = UIActivityViewController(activityItems: [image!], applicationActivities: nil)
+        
+        activityViewController.popoverPresentationController?.sourceView = self.view // so that iPads won't crash
+        activityViewController.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+    activityViewController.excludedActivityTypes = [ UIActivity.ActivityType.airDrop, UIActivity.ActivityType.postToFacebook ]
+
+        
+       present(activityViewController, animated: true, completion: nil)
+    }
+}
+
+extension PictureSharingViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+            img.image = image
+        }
+        dismiss(animated: true, completion: nil)
+    }
 }
